50d29eeb92fa2318d31cb570a92798fbb42104f2
Blagovest-Todorov <blagovest.stod@abv.bg>
2020-11-17 18:59:24 +0200

    new code

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace arrayNewReversedNumbers
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("enter please array size = ");
+            int n = int.Parse(Console.ReadLine());
+            int[] numbers = new int[n];
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                numbers[i] = int.Parse(Console.ReadLine()); 
+            }
+            string numbersToString = string.Join(", ", numbers);
+            Console.WriteLine(numbersToString);
+            
+
+        }
+    }
+}
